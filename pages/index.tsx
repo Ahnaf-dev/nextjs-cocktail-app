49467f3181d6cf421cddf4f6f753017feef59f87
Ahnaf-dev <ahnafbari99@gmail.com>
2022-11-28 14:56:38 -0700

    adding styled components theming and page routers

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
 
-const Div = styled.div`
-  background-color: red;
-
-  h1 {
-    color: white;
-  }
-`;
-
 export default function Home() {
-  return (
-    <Div>
-      <h1>Hello World</h1>;
-    </Div>
-  );
+  return <h1>Hello World</h1>;
 }
---
+++ theme/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  palette: {
+    main: "#303648",
+    secondary: "#232A3A",
+    accent: "#7ED131",
+  },
+};
+
+export default theme;
