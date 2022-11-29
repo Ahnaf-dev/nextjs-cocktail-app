10d49966b8afa0fa7cb2825573cbd37b306cde50
Ahnaf-dev <ahnafbari99@gmail.com>
2022-11-28 19:29:33 -0700

    added testing for search functionality using React Testing Library FireEvent and Jest Mock Function to mock API calls

--- tests/home.test.js
+++ tests/home.test.js
@@ -1,6 +1,7 @@
 import Home from "../pages/index";
 import Header from "../components/Header";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 import { ThemeProvider } from "styled-components";
 import theme from "../theme/theme";
 
@@ -28,6 +29,64 @@ describe("Home Component", () => {
 
     it("should display cocktail cards", () => {
       expect(screen.queryByText("Rendered Glass")).toBeVisible();
+    });
+  });
+
+  describe("When user searches on searchbar", () => {
+    const cocktails = [
+      {
+        id: 1,
+        glass: "FilterA Glass",
+        drink: "Hydrated",
+        image: "",
+        alcoholic: "alcoholic",
+      },
+      {
+        id: 1,
+        glass: "FilterB Glass",
+        drink: "Hydrated",
+        image: "",
+        alcoholic: "alcoholic",
+      },
+    ];
+
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            drinks: [
+              {
+                idDrink: "one",
+                strGlass: "FilterA",
+                strDrink: "drink1",
+                strDrinkThumb: "",
+                strAlcoholic: "true",
+              },
+            ],
+          }),
+      })
+    );
+
+    beforeEach(() => {
+      render(
+        <ThemeProvider theme={theme}>
+          <Home cocktails={cocktails} />
+        </ThemeProvider>
+      );
+    });
+
+    it("should display cocktail cards that matches the search term", async () => {
+      const input = screen.queryByPlaceholderText("Search For Cocktail");
+      await act(async () => {
+        fireEvent.change(input, { target: { value: "FilterA" } });
+      });
+
+      await waitFor(
+        () => {
+          expect(screen.queryByText("FilterA")).toBeVisible();
+        },
+        { interval: 1000 }
+      );
     });
   });
 });
